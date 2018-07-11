7f01d49c4436598e817c8ee224da1798d7a039d8
Jim Speaker <jim@IONTECH.org>
2018-07-11 14:38:02 -0700

    baseline for kata

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,9 +1,27 @@
-﻿
+﻿var Kata = Kata || {};
+
+Kata.FizzBuzz = function () {
+  // ReSharper disable once CallerCalleeUsing
+  if (!(this instanceof arguments.callee)) {
+    return new arguments.callee();
+  }
+
+};
+
+// ReSharper disable once NativeTypePrototypeExtending
+Number.prototype.evenlyDivisibleBy = function(dividend) {
+  return this % dividend === 0;
+};
 
 describe("GivenFoo",
   function () {
     it("WhenCallingBar_ThenItShouldBaz",
       function () {
-        expect(1).toEqual(1);
+        // arrange
+
+        // act
+
+        // assert
+        expect("bar").toEqual("baz");
       });
   });
